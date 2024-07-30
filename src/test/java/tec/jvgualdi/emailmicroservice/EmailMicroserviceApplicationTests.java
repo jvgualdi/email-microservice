13353c5eb3e8da74125d54911536db888fc6689b
jvgualdi <jvgualdi1@gmail.com>
2024-07-30 13:59:57 -0300

    Initial Project

---
+++ src/test/java/tec/jvgualdi/emailmicroservice/EmailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.jvgualdi.emailmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
